4982727be5f238603d8bbc2bd2b7f1fdb197fde1
1EmirSimon <emirsimon76@gmail.com>
2026-05-23 20:12:54 -0300

    Actualizacion de descargas

--- script.js
+++ script.js
@@ -589,3 +589,33 @@ window.onload = () => {
     startIntro();
 
 };
+
+// =========================================================
+// HERO MAIN GAME
+// =========================================================
+
+window.openMeteorHero = () => {
+
+    const meteorGame = {
+
+        name: "Meteor Fighters",
+
+        author: "Dreaming Rose",
+
+        description:
+            "Enfréntate a dinosaurios y criaturas prehistóricas en intensas batallas. ¡Desata combos devastadores y domina el campo de batalla jurásico!",
+
+        cover:
+            "assets/images/Meteor Fighters.png",
+
+        stars: 999,
+
+        fileURL:
+            "https://drive.google.com/drive/folders/1lfdN5JWkNyDRUOx4O1VFtJYEO3SURPXE?usp=sharing"
+
+
+    };
+
+    openGameModal(meteorGame);
+
+};
